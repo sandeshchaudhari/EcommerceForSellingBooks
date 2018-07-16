360f7c4ebb7798e2d96d45d859486392645c44f8
sandesh <sandeshchaudhari20@gmail.com>
2018-07-16 15:32:16 +0530

    Products CRUD completed

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('products', 'ProductsController');
+
+Route::get('/', [
+    'uses' => 'FrontEndController@index',
+    'as' => 'index'
+]);
+Route::get('/product/{id}', [
+    'uses' => 'FrontEndController@singleProduct',
+    'as' => 'product.single'
+]);
